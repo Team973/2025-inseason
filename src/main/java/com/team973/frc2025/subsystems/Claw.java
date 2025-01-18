ad1a4760ceb5b4af360e07c4f50028028232fb98
cameron <cameron.breitung@gmail.com>
2025-01-18 11:07:23 -0800

    added score action and new auto comand

--- src/main/java/com/team973/frc2025/subsystems/Claw.java
+++ src/main/java/com/team973/frc2025/subsystems/Claw.java
@@ -14,6 +14,9 @@ public class Claw implements Subsystem {
   private final GreyTalonFX m_motorLeft;
   private final DigitalInput m_sensor = new DigitalInput(0);
   private ControlStatus m_mode = ControlStatus.Stop;
+  private ControlStatus m_lastMode = ControlStatus.Stop;
+  private double m_leftTargetPostion = 0;
+  private double m_rightTargetMotion = 0;
 
   public Claw(Logger logger) {
     m_logger = logger;
@@ -22,10 +25,33 @@ public class Claw implements Subsystem {
 
     TalonFXConfiguration rightMotorConfig = new TalonFXConfiguration();
     rightMotorConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    rightMotorConfig.Slot0.kS = 0.0;
+    rightMotorConfig.Slot0.kV = 0.0;
+    rightMotorConfig.Slot0.kA = 0.0;
+    rightMotorConfig.Slot0.kP = 1.0;
+    rightMotorConfig.Slot0.kI = 0.0;
+    rightMotorConfig.Slot0.kD = 0.0;
+    rightMotorConfig.MotionMagic.MotionMagicCruiseVelocity = 8;
+    rightMotorConfig.MotionMagic.MotionMagicAcceleration = 16;
+    rightMotorConfig.MotionMagic.MotionMagicJerk = 160;
     m_motorRight.setConfig(rightMotorConfig);
     TalonFXConfiguration leftMotorConfig = new TalonFXConfiguration();
+    leftMotorConfig.Slot0.kS = 0.0;
+    leftMotorConfig.Slot0.kV = 0.0;
+    leftMotorConfig.Slot0.kA = 0.0;
+    leftMotorConfig.Slot0.kP = 1.0;
+    leftMotorConfig.Slot0.kI = 0.0;
+    leftMotorConfig.Slot0.kD = 0.0;
+    leftMotorConfig.MotionMagic.MotionMagicCruiseVelocity = 8;
+    leftMotorConfig.MotionMagic.MotionMagicAcceleration = 16;
+    leftMotorConfig.MotionMagic.MotionMagicJerk = 160;
     leftMotorConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
     m_motorLeft.setConfig(leftMotorConfig);
+  }
+
+  public boolean motorAtTarget() {
+    return (Math.abs(m_leftTargetPostion - m_motorLeft.getPosition().getValueAsDouble()) < 0.1
+        && Math.abs(m_rightTargetMotion - m_motorRight.getPosition().getValueAsDouble()) < 0.1);
   }
 
   public boolean sensorSeeCoral() {
@@ -37,6 +63,7 @@ public class Claw implements Subsystem {
     Shoot,
     Stop,
     Retract,
+    Score,
   }
 
   @Override
@@ -47,8 +74,8 @@ public class Claw implements Subsystem {
           m_motorRight.setControl(ControlMode.DutyCycleOut, 0);
           m_motorLeft.setControl(ControlMode.DutyCycleOut, 0);
         } else if (!sensorSeeCoral()) {
-          m_motorRight.setControl(ControlMode.DutyCycleOut, 0.05);
-          m_motorLeft.setControl(ControlMode.DutyCycleOut, 0.05);
+          m_motorRight.setControl(ControlMode.DutyCycleOut, 0.1);
+          m_motorLeft.setControl(ControlMode.DutyCycleOut, 0.1);
         }
         break;
       case Shoot:
@@ -58,6 +85,14 @@ public class Claw implements Subsystem {
       case Stop:
         m_motorRight.setControl(ControlMode.DutyCycleOut, 0);
         m_motorLeft.setControl(ControlMode.DutyCycleOut, 0);
+        break;
+      case Score:
+        if (m_lastMode != m_mode) {
+          m_rightTargetMotion = m_motorRight.getPosition().getValueAsDouble() + 4.5;
+          m_leftTargetPostion = m_motorLeft.getPosition().getValueAsDouble() + 4.5;
+        }
+        m_motorRight.setControl(ControlMode.MotionMagicVoltage, m_rightTargetMotion);
+        m_motorLeft.setControl(ControlMode.MotionMagicVoltage, m_leftTargetPostion);
         break;
       case Retract:
         m_motorRight.setControl(ControlMode.DutyCycleOut, -0.1);
@@ -75,6 +110,9 @@ public class Claw implements Subsystem {
     m_logger.log("sensorSeeCoral", sensorSeeCoral());
     m_motorRight.log();
     m_motorLeft.log();
+    m_logger.log("target postion left", m_leftTargetPostion);
+    m_logger.log("target postion right", m_rightTargetMotion);
+    m_logger.log("target rotations hit", motorAtTarget());
   }
 
   @Override
