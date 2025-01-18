82987ff4ba89cd5d1f73f4d3a0ad6c9b27b1a4ea
NolanKnight <nolan.r.knight@gmail.com>
2025-01-18 13:49:30 -0800

    added event marker listening

--- src/main/java/com/team973/frc2025/auto/modes/TestAuto.java
+++ src/main/java/com/team973/frc2025/auto/modes/TestAuto.java
@@ -1,8 +1,11 @@
 package com.team973.frc2025.auto.modes;
 
+import com.team973.frc2025.auto.commands.ClawCommand;
 import com.team973.frc2025.auto.commands.DriveTrajectoryCommand;
+import com.team973.frc2025.subsystems.Claw;
 import com.team973.frc2025.subsystems.DriveController;
 import com.team973.lib.util.AutoMode;
+import com.team973.lib.util.CommandOnEvent;
 import com.team973.lib.util.Logger;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -12,10 +15,13 @@ public class TestAuto extends AutoMode {
     super(
         logger,
         new Pose2d(7.6, 4, Rotation2d.fromDegrees(180)),
-        new DriveTrajectoryCommand(logger.subLogger("PF-HP-L2"), drive, "PF-HP-L2"),
-        new DriveTrajectoryCommand(logger.subLogger("PF-L2-HP"), drive, "PF-L2-HP"),
-        new DriveTrajectoryCommand(logger.subLogger("PF-HP-R2"), drive, "PF-HP-R2"),
-        new DriveTrajectoryCommand(logger.subLogger("PF-R2-HP"), drive, "PF-R2-HP"));
+        new DriveTrajectoryCommand(
+            drive,
+            "PF-HP-L2",
+            new CommandOnEvent("Shoot", new ClawCommand(null, Claw.ControlStatus.Shoot))),
+        new DriveTrajectoryCommand(drive, "PF-L2-HP"),
+        new DriveTrajectoryCommand(drive, "PF-HP-R2"),
+        new DriveTrajectoryCommand(drive, "PF-R2-HP"));
   }
 
   public String getName() {
