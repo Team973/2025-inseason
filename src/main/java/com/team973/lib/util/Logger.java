c604df3cb544a4b48c43bed94f945843bb33560d
NolanKnight <nolan.r.knight@gmail.com>
2025-01-19 16:07:54 -0800

    added seconds per log option to logger

--- src/main/java/com/team973/lib/util/Logger.java
+++ src/main/java/com/team973/lib/util/Logger.java
@@ -5,13 +5,31 @@ import dev.doglog.DogLogOptions;
 
 public class Logger {
   private final String m_prefix;
+  private final double m_secondsPerLog;
 
-  public Logger(String prefix) {
+  private double m_nextAllowedLogTime;
+
+  public Logger(String prefix, double secondsPerLog) {
     m_prefix = prefix;
+    m_secondsPerLog = secondsPerLog;
+    m_nextAllowedLogTime = secondsPerLog;
 
     DogLog.setOptions(new DogLogOptions().withNtPublish(true));
     // TODO: solve why this doesn't work
     // DogLog.setPdh(new PowerDistribution());
+  }
+
+  public Logger(String prefix) {
+    this(prefix, 0.0);
+  }
+
+  private boolean logAllowed() {
+    if (Conversions.Time.getSecTime() > m_nextAllowedLogTime) {
+      m_nextAllowedLogTime += m_secondsPerLog;
+      return true;
+    }
+
+    return false;
   }
 
   public String getPrefix() {
@@ -19,38 +37,58 @@ public class Logger {
   }
 
   public Logger subLogger(String prefix) {
-    return new Logger(m_prefix + "/" + prefix);
+    return new Logger(m_prefix + "/" + prefix, m_secondsPerLog);
+  }
+
+  public Logger subLogger(String prefix, double secondsPerLog) {
+    return new Logger(m_prefix + "/" + prefix, secondsPerLog);
   }
 
   public void log(String key, double value) {
-    DogLog.log(m_prefix + "/" + key, value);
+    if (logAllowed()) {
+      DogLog.log(m_prefix + "/" + key, value);
+    }
   }
 
   public void log(String key, String value) {
-    DogLog.log(m_prefix + "/" + key, value);
+    if (logAllowed()) {
+      DogLog.log(m_prefix + "/" + key, value);
+    }
   }
 
   public void log(String key, int value) {
-    DogLog.log(m_prefix + "/" + key, value);
+    if (logAllowed()) {
+      DogLog.log(m_prefix + "/" + key, value);
+    }
   }
 
   public void log(String key, boolean value) {
-    DogLog.log(m_prefix + "/" + key, value);
+    if (logAllowed()) {
+      DogLog.log(m_prefix + "/" + key, value);
+    }
   }
 
   public void log(String key, double[] value) {
-    DogLog.log(m_prefix + "/" + key, value);
+    if (logAllowed()) {
+      DogLog.log(m_prefix + "/" + key, value);
+    }
   }
 
   public void log(String key, String[] value) {
-    DogLog.log(m_prefix + "/" + key, value);
+    if (logAllowed()) {
+      DogLog.log(m_prefix + "/" + key, value);
+    }
   }
 
   public void log(String key, int[] value) {
-    DogLog.log(m_prefix + "/" + key, value);
+    if (logAllowed()) {
+      DogLog.log(m_prefix + "/" + key, value);
+    }
   }
 
   public void log(String key, boolean[] value) {
-    DogLog.log(m_prefix + "/" + key, value);
+    if (logAllowed()) {
+      DogLog.log(m_prefix + "/" + key, value);
+    }
   }
 }
