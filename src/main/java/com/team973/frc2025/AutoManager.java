39b21a6f3bf6c56f4d63317d285a72ba1e6885bf
NolanKnight <nolan.r.knight@gmail.com>
2025-01-19 16:05:49 -0800

    drive test auto

--- src/main/java/com/team973/frc2025/AutoManager.java
+++ src/main/java/com/team973/frc2025/AutoManager.java
@@ -1,6 +1,7 @@
 package com.team973.frc2025;
 
 import com.team973.frc2025.auto.modes.ClawTestAuto;
+import com.team973.frc2025.auto.modes.DriveTestAuto;
 import com.team973.frc2025.auto.modes.NoAuto;
 import com.team973.frc2025.auto.modes.TaxiAuto;
 import com.team973.frc2025.auto.modes.TestAuto;
@@ -21,14 +22,17 @@ public class AutoManager {
   private final AutoMode m_taxiAuto;
   private final AutoMode m_clawTestAuto;
   private final AutoMode m_testAuto;
+  private final AutoMode m_driveTestAuto;
 
   public AutoManager(Logger logger, DriveController drive, Claw claw) {
     m_noAuto = new NoAuto(logger);
     m_taxiAuto = new TaxiAuto(logger.subLogger("taxi"), drive, claw);
     m_clawTestAuto = new ClawTestAuto(logger.subLogger("claw"), claw);
-    m_testAuto = new TestAuto(logger, drive, claw);
+    m_testAuto = new TestAuto(logger.subLogger("test"), drive, claw);
+    m_driveTestAuto = new DriveTestAuto(logger.subLogger("driveTest"), drive);
 
-    m_availableAutos = Arrays.asList(m_noAuto, m_taxiAuto, m_clawTestAuto, m_testAuto);
+    m_availableAutos =
+        Arrays.asList(m_noAuto, m_taxiAuto, m_clawTestAuto, m_testAuto, m_driveTestAuto);
   }
 
   public void increment() {
