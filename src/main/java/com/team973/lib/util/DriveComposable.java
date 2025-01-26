80a79557cc5a2ee8f6327ad00bc770cfa0c61c5b
NolanKnight <nolan.r.knight@gmail.com>
2025-01-25 16:09:25 -0800

    simplified compose method

--- src/main/java/com/team973/lib/util/DriveComposable.java
+++ src/main/java/com/team973/lib/util/DriveComposable.java
@@ -5,39 +5,16 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 public abstract class DriveComposable {
   private boolean m_firstRun = true;
 
-  public static class Config {
-    public enum Composable {
-      A,
-      B
-    }
-
-    public Composable translation = Composable.A;
-    public Composable rotation = Composable.A;
-  }
-
   public static DriveComposable compose(
-      DriveComposable composableA, DriveComposable composableB, Config config) {
+      DriveComposable translationProvider, DriveComposable rotationProvider) {
     return new DriveComposable() {
       @Override
       public ChassisSpeeds getOutput() {
         ChassisSpeeds output = new ChassisSpeeds();
 
-        ChassisSpeeds outputA = composableA.getOutput();
-        ChassisSpeeds outputB = composableB.getOutput();
-
-        if (config.translation == Config.Composable.A) {
-          output.vxMetersPerSecond = outputA.vxMetersPerSecond;
-          output.vyMetersPerSecond = outputA.vyMetersPerSecond;
-        } else {
-          output.vxMetersPerSecond = outputB.vxMetersPerSecond;
-          output.vyMetersPerSecond = outputB.vyMetersPerSecond;
-        }
-
-        if (config.rotation == Config.Composable.A) {
-          output.omegaRadiansPerSecond = outputA.omegaRadiansPerSecond;
-        } else {
-          output.omegaRadiansPerSecond = outputB.omegaRadiansPerSecond;
-        }
+        output.vxMetersPerSecond = translationProvider.getOutput().vxMetersPerSecond;
+        output.vyMetersPerSecond = translationProvider.getOutput().vyMetersPerSecond;
+        output.omegaRadiansPerSecond = rotationProvider.getOutput().omegaRadiansPerSecond;
 
         return output;
       }
