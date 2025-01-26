ec7b98f8820233897301872e9a404231da932cd4
cameron <cameron.breitung@gmail.com>
2025-01-26 13:44:29 -0800

    started with alge

--- src/main/java/com/team973/frc2025/subsystems/Claw.java
+++ src/main/java/com/team973/frc2025/subsystems/Claw.java
@@ -2,6 +2,7 @@ package com.team973.frc2025.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.team973.frc2025.shared.RobotInfo;
 import com.team973.lib.devices.GreyTalonFX;
 import com.team973.lib.devices.GreyTalonFX.ControlMode;
 import com.team973.lib.util.Logger;
@@ -11,18 +12,24 @@ import edu.wpi.first.wpilibj.DigitalInput;
 public class Claw implements Subsystem {
   private final Logger m_logger;
   private final GreyTalonFX m_clawMotor;
+  private final GreyTalonFX m_algeMotor;
   private final DigitalInput m_bannerSensorFront = new DigitalInput(1);
   private final DigitalInput m_bannerSensorBack = new DigitalInput(0);
+  private final DigitalInput m_algeSensor = new DigitalInput(2);
   private ControlStatus m_mode = ControlStatus.Stop;
 
   public Claw(Logger logger) {
     m_logger = logger;
-    m_clawMotor = new GreyTalonFX(36, "Canivore", new Logger("ClawMotor"));
+    m_algeMotor = new GreyTalonFX(99, RobotInfo.CANIVORE_CANBUS, new Logger("AlgeMotor"));
+    m_clawMotor = new GreyTalonFX(36, RobotInfo.CANIVORE_CANBUS, new Logger("ClawMotor"));
 
     TalonFXConfiguration clawMotorConfig = new TalonFXConfiguration();
     clawMotorConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
     m_clawMotor.setConfig(clawMotorConfig);
-    
+  }
+
+  public boolean algeSensorSeesAlge() {
+    return m_algeSensor.get();
   }
 
   public boolean frontBannerSensorSeesCoral() {
@@ -34,6 +41,8 @@ public class Claw implements Subsystem {
   }
 
   public static enum ControlStatus {
+    AlgeIntakeHold,
+    AlgeShoot,
     IntakeAndHold,
     Shoot,
     Stop,
@@ -43,6 +52,16 @@ public class Claw implements Subsystem {
   @Override
   public void update() {
     switch (m_mode) {
+      case AlgeIntakeHold:
+        if (!algeSensorSeesAlge()) {
+          m_algeMotor.setControl(ControlMode.DutyCycleOut, 1);
+        } else {
+          m_algeMotor.setControl(ControlMode.DutyCycleOut, 0);
+        }
+        break;
+      case AlgeShoot:
+        m_algeMotor.setControl(ControlMode.DutyCycleOut, 1);
+        break;
       case IntakeAndHold:
         if (frontBannerSensorSeesCoral() && !backBannerSensorSeesCoral()) {
           m_clawMotor.setControl(ControlMode.DutyCycleOut, 0);
@@ -75,6 +94,7 @@ public class Claw implements Subsystem {
     m_logger.log("backBannerSensor", backBannerSensorSeesCoral());
     m_logger.log("frontBannerSensor", frontBannerSensorSeesCoral());
     m_clawMotor.log();
+    m_algeMotor.log();
   }
 
   @Override
