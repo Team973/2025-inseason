d811540bb4ad426b7e2db04fca53df0318cb324a
astrydern <astrydern@gmail.com>
2025-02-13 18:42:33 -0800

    experimental conveyor velocity code

--- src/main/java/com/team973/frc2025/subsystems/Conveyor.java
+++ src/main/java/com/team973/frc2025/subsystems/Conveyor.java
@@ -2,6 +2,7 @@ package com.team973.frc2025.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.team973.lib.devices.GreyTalonFX;
 import com.team973.lib.util.Logger;
 import com.team973.lib.util.Subsystem;
@@ -18,8 +19,35 @@ public class Conveyor implements Subsystem {
   public Conveyor(Logger logger) {
     m_logger = logger;
     m_conveyor = new GreyTalonFX(36, "Canivore", m_logger.subLogger("conveyor motor"));
-    motorConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-    m_conveyor.setConfig(motorConfig);
+    m_conveyor.setConfig(defaultMotorConfig());
+  }
+
+  private static TalonFXConfiguration defaultMotorConfig() {
+    TalonFXConfiguration defaultMotorConfig = new TalonFXConfiguration();
+    defaultMotorConfig.Slot0.kS = 0.0;
+    defaultMotorConfig.Slot0.kV = 0.05;
+    defaultMotorConfig.Slot0.kA = 0.0;
+    defaultMotorConfig.Slot0.kP = 0.0;
+    defaultMotorConfig.Slot0.kI = 0.0;
+    defaultMotorConfig.Slot0.kD = 0.0;
+    defaultMotorConfig.MotionMagic.MotionMagicCruiseVelocity = 32;
+    defaultMotorConfig.MotionMagic.MotionMagicAcceleration = 32;
+    defaultMotorConfig.MotionMagic.MotionMagicJerk = 160;
+    // slot 1 is for velocity
+    defaultMotorConfig.Slot1.kS = 0.0;
+    defaultMotorConfig.Slot1.kV = 0.05;
+    defaultMotorConfig.Slot1.kA = 0.0;
+    defaultMotorConfig.Slot1.kP = 0.0;
+    defaultMotorConfig.Slot1.kI = 0.0;
+    defaultMotorConfig.Slot1.kD = 0.0;
+    defaultMotorConfig.CurrentLimits.StatorCurrentLimit = 70;
+    defaultMotorConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+    defaultMotorConfig.CurrentLimits.SupplyCurrentLimit = 40;
+    defaultMotorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    defaultMotorConfig.ClosedLoopRamps.VoltageClosedLoopRampPeriod = 0.02;
+    defaultMotorConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    defaultMotorConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+    return defaultMotorConfig;
   }
 
   public enum ControlMode {
