d56a74c2342109bfb0ecc7942198199a47b000b2
cameron <cameron.breitung@gmail.com>
2025-02-16 13:36:20 -0800

    made new signaler for coral

--- src/main/java/com/team973/frc2025/Robot.java
+++ src/main/java/com/team973/frc2025/Robot.java
@@ -6,7 +6,6 @@ package com.team973.frc2025;
 
 import com.team973.frc2025.shared.RobotInfo;
 import com.team973.frc2025.subsystems.Arm;
-import com.team973.frc2025.subsystems.BlinkingSignaler;
 import com.team973.frc2025.subsystems.CANdleManger;
 import com.team973.frc2025.subsystems.Claw;
 import com.team973.frc2025.subsystems.Climb;
@@ -46,8 +45,10 @@ public class Robot extends TimedRobot {
   private final Arm m_arm = new Arm(m_logger.subLogger("Arm", 0.2));
 
   private final CANdleManger m_CaNdleManger = new CANdleManger(new Logger("candle manger"));
-  private final BlinkingSignaler m_lowBatterySignaler =
-      new BlinkingSignaler(RobotInfo.Colors.OARNGE, RobotInfo.Colors.OFF, 300, 1);
+
+  private final SolidSignaler m_lowBatterySignaler = new SolidSignaler(RobotInfo.Colors.ORANGE, 1);
+  private final SolidSignaler m_coralInclawBlinker = new SolidSignaler(RobotInfo.Colors.GREEN, 2);
+
   private final SolidSignaler m_ledOff = new SolidSignaler(RobotInfo.Colors.OFF, 100);
   private final AutoManager m_autoManager =
       new AutoManager(m_logger.subLogger("auto"), m_driveController, m_claw);
@@ -104,6 +105,8 @@ public class Robot extends TimedRobot {
     m_driveController.startOdometrey();
     m_ledOff.setEnabled(true);
     m_CaNdleManger.addSignaler(m_lowBatterySignaler);
+    m_CaNdleManger.addSignaler(m_ledOff);
+    m_CaNdleManger.addSignaler(m_coralInclawBlinker);
     m_stick = new Joystick(2, Joystick.Type.XboxController, m_logger.subLogger("sticks"));
     m_teststick = new Joystick(3, Joystick.Type.XboxController, m_logger.subLogger("sticks"));
   }
@@ -144,6 +147,12 @@ public class Robot extends TimedRobot {
     }
     if (RobotController.getBatteryVoltage() > 12.1) {
       m_lowBatterySignaler.setEnabled(false);
+    }
+    if (m_claw.sensorSeeCoral() == true) {
+      m_coralInclawBlinker.setEnabled(true);
+    }
+    if (m_claw.sensorSeeCoral() == false) {
+      m_coralInclawBlinker.setEnabled(false);
     }
 
     logSubsystems();
@@ -225,19 +234,19 @@ public class Robot extends TimedRobot {
     } else if (m_coDriverStick.getYButton()) {
       m_claw.setControl(Claw.ControlStatus.Retract);
     }
-    // if (m_coDriverStick.getPOVTop()) {
-    //   m_elevator.setTargetPostion(Elevator.Presets.LEVEL_4);
-    // } else if (m_coDriverStick.getPOVLeft()) {
-    //   m_elevator.setTargetPostion(Elevator.Presets.LEVEL_3);
-    // } else if (m_coDriverStick.getPOVRight()) {
-    //   m_elevator.setTargetPostion(Elevator.Presets.LEVEL_2);
-    // } else if (m_coDriverStick.getPOVBottom()) {
-    //   m_elevator.setTargetPostion(Elevator.Presets.LEVEL_1);
-    // } else if (m_coDriverStick.getLeftBumperButton()) {
-    //   m_elevator.setmotorManualOutput(m_coDriverStick.getLeftYAxis());
-    // } else {
-    //   m_elevator.setModeOff();
-    // }
+    if (m_coDriverStick.getPOVTop()) {
+      m_elevator.setTargetPostion(Elevator.Presets.LEVEL_4);
+    } else if (m_coDriverStick.getPOVLeft()) {
+      m_elevator.setTargetPostion(Elevator.Presets.LEVEL_3);
+    } else if (m_coDriverStick.getPOVRight()) {
+      m_elevator.setTargetPostion(Elevator.Presets.LEVEL_2);
+    } else if (m_coDriverStick.getPOVBottom()) {
+      m_elevator.setTargetPostion(Elevator.Presets.LEVEL_1);
+    } else if (m_coDriverStick.getLeftBumperButton()) {
+      m_elevator.setmotorManualOutput(m_coDriverStick.getLeftYAxis());
+    } else {
+      m_elevator.setModeOff();
+    }
 
     if (m_coDriverStick.getLeftBumperButton()) {
       m_arm.setArmTargetDeg(Arm.HIGH_POSTION_DEG);
--- src/main/java/com/team973/frc2025/shared/RobotInfo.java
+++ src/main/java/com/team973/frc2025/shared/RobotInfo.java
@@ -142,7 +142,7 @@ public final class RobotInfo {
     public static final Color GREEN = new Color(0, 255, 0);
     public static final Color BLUE = new Color(0, 0, 255);
     public static final Color OFF = new Color(0, 0, 0);
-    public static final Color OARNGE = new Color(255, 165, 0);
+    public static final Color ORANGE = new Color(255, 100, 25);
     public static final Color PINK = new Color(255, 105, 180);
     public static final Color PURPLE = new Color(128, 0, 128);
     public static final Color CYAN = new Color(0, 255, 255);
