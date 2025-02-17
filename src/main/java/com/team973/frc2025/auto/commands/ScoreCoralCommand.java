7c642b7e35c98ed618ef01d2b609feb1b263fcbb
NolanKnight <nolan.r.knight@gmail.com>
2025-02-17 15:20:19 -0800

    score coral auto command

---
+++ src/main/java/com/team973/frc2025/auto/commands/ScoreCoralCommand.java
@@ -0,0 +1,51 @@
+package com.team973.frc2025.auto.commands;
+
+import com.team973.frc2025.subsystems.DriveController;
+import com.team973.frc2025.subsystems.DriveController.ControllerOption;
+import com.team973.frc2025.subsystems.Superstructure;
+import com.team973.lib.util.AutoCommand;
+
+public class ScoreCoralCommand extends AutoCommand {
+  private final DriveController m_drive;
+  private final Superstructure m_superstructure;
+
+  private final int m_targetReefFace;
+  private final int m_targetReefLevel;
+
+  public ScoreCoralCommand(
+      DriveController drive,
+      Superstructure superstructure,
+      int targetReefFace,
+      int targetReefLevel) {
+    m_drive = drive;
+    m_superstructure = superstructure;
+
+    m_targetReefFace = targetReefFace;
+    m_targetReefLevel = targetReefLevel;
+  }
+
+  @Override
+  public void init() {
+    m_drive.setControllerOption(ControllerOption.DriveWithLimelight);
+    m_drive.getDriveWithLimelight().setTargetReefFace(m_targetReefFace);
+    m_superstructure.setTargetReefLevel(m_targetReefLevel);
+    m_drive
+        .getDriveWithLimelight()
+        .targetReefPosition(
+            () -> m_superstructure.readyToScore(), () -> m_superstructure.finishedScoring());
+  }
+
+  @Override
+  public void run() {}
+
+  @Override
+  public void log() {}
+
+  @Override
+  public boolean isCompleted() {
+    return m_drive.getDriveWithLimelight().getTargetingComplete();
+  }
+
+  @Override
+  public void postComplete(boolean interrupted) {}
+}
