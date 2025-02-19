de21dd99ae2ead1d32bfe853ebb8927b0fc0d1e8
NolanKnight <nolan.r.knight@gmail.com>
2025-02-18 18:28:49 -0800

    fixed auto error

--- src/main/java/com/team973/frc2025/auto/commands/ScoreCoralCommand.java
+++ src/main/java/com/team973/frc2025/auto/commands/ScoreCoralCommand.java
@@ -3,19 +3,21 @@ package com.team973.frc2025.auto.commands;
 import com.team973.frc2025.subsystems.DriveController;
 import com.team973.frc2025.subsystems.DriveController.ControllerOption;
 import com.team973.frc2025.subsystems.Superstructure;
+import com.team973.frc2025.subsystems.composables.DriveWithLimelight.TargetReefFace;
 import com.team973.lib.util.AutoCommand;
 
 public class ScoreCoralCommand extends AutoCommand {
   private final DriveController m_drive;
   private final Superstructure m_superstructure;
 
-  private final int m_targetReefFace;
+  private final TargetReefFace m_targetReefFace;
+
   private final int m_targetReefLevel;
 
   public ScoreCoralCommand(
       DriveController drive,
       Superstructure superstructure,
-      int targetReefFace,
+      TargetReefFace targetReefFace,
       int targetReefLevel) {
     m_drive = drive;
     m_superstructure = superstructure;
