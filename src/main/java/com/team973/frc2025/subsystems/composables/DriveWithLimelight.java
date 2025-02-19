0009e3f42c2bf58f824fa5c6c2cc50c04a3d4a13
Andrew Nelson <andrew2nelson@Andrews-MacBook-Air.local>
2025-02-18 18:18:11 -0800

    Make approach less aggressive

--- src/main/java/com/team973/frc2025/subsystems/composables/DriveWithLimelight.java
+++ src/main/java/com/team973/frc2025/subsystems/composables/DriveWithLimelight.java
@@ -134,10 +134,10 @@ public class DriveWithLimelight extends DriveComposable {
     double controlPeriodSeconds = 1.0 / RobotInfo.DriveInfo.STATUS_SIGNAL_FREQUENCY;
     m_xController =
         new ProfiledPIDController(
-            5.0, 0, 0, new TrapezoidProfile.Constraints(1.0, 0.25), controlPeriodSeconds);
+            4.0, 0, 0, new TrapezoidProfile.Constraints(0.8, 0.2), controlPeriodSeconds);
     m_yController =
         new ProfiledPIDController(
-            5.0, 0, 0, new TrapezoidProfile.Constraints(1.0, 0.25), controlPeriodSeconds);
+            4.0, 0, 0, new TrapezoidProfile.Constraints(0.8, 0.2), controlPeriodSeconds);
     m_thetaController =
         new ProfiledPIDController(
             8.0, 0, 0, new TrapezoidProfile.Constraints(1.0, 0.6), controlPeriodSeconds);
