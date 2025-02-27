b13b7f92f42b46e6a1061e902542e0c486b91073
NolanKnight <nolan.r.knight@gmail.com>
2025-02-27 15:40:25 -0800

    updated arm stow angle

--- src/main/java/com/team973/frc2025/subsystems/Arm.java
+++ src/main/java/com/team973/frc2025/subsystems/Arm.java
@@ -33,7 +33,7 @@ public class Arm implements Subsystem {
   private static final double LEVEL_THREE_POSITION_DEG = 64.0;
   private static final double LEVEL_TWO_POSITION_DEG = -58.0; // -70.0;
   private static final double LEVEL_ONE_POSITION_DEG = -59.0; // -70.0;
-  public static final double CORAL_STOW_POSITION_DEG = -92.0;
+  public static final double CORAL_STOW_POSITION_DEG = -91.0;
 
   private static final double ALGAE_HIGH_POSITION_DEG = 38.0;
   private static final double ALGAE_LOW_POSITION_DEG = -61.0;
