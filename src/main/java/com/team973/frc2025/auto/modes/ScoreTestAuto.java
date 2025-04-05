7b6982cac2b1109d3fc1bb5a254b70a547de0912
NolanKnight <nolan.r.knight@gmail.com>
2025-04-04 17:29:43 -0700

    made score test auto

---
+++ src/main/java/com/team973/frc2025/auto/modes/ScoreTestAuto.java
@@ -0,0 +1,27 @@
+package com.team973.frc2025.auto.modes;
+
+import com.team973.frc2025.auto.commands.DriveTrajectoryCommand;
+import com.team973.frc2025.auto.commands.ScoreCoralCommand;
+import com.team973.frc2025.subsystems.DriveController;
+import com.team973.frc2025.subsystems.Superstructure;
+import com.team973.frc2025.subsystems.Superstructure.ReefLevel;
+import com.team973.frc2025.subsystems.composables.DriveWithLimelight.ReefFace;
+import com.team973.frc2025.subsystems.composables.DriveWithLimelight.ReefSide;
+import com.team973.lib.util.AutoMode;
+import com.team973.lib.util.Logger;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class ScoreTestAuto extends AutoMode {
+  public ScoreTestAuto(Logger logger, DriveController drive, Superstructure superstructure) {
+    super(
+        logger,
+        new Pose2d(17.13, 3.95, Rotation2d.fromDegrees(0)),
+        new ScoreCoralCommand(drive, superstructure, ReefFace.A, ReefLevel.L_4, ReefSide.Left),
+        new DriveTrajectoryCommand(drive, "Score-Test", logger.subLogger("Score-Test")));
+  }
+
+  public String getName() {
+    return "Score Test Auto";
+  }
+}
