6e9c6a0e200593062f77ccac7091a4be3c596cfa
NolanKnight <nolan.r.knight@gmail.com>
2025-04-17 11:37:34 -0500

    zeroed arm and wrist and tuned presets

--- src/main/java/com/team973/frc2025/subsystems/Arm.java
+++ src/main/java/com/team973/frc2025/subsystems/Arm.java
@@ -29,7 +29,7 @@ public class Arm implements Subsystem {
 
   private static final double HORIZONTAL_POSITION_DEG = 0.0;
 
-  private static final double LEVEL_FOUR_POSITION_DEG = 61.0;
+  private static final double LEVEL_FOUR_POSITION_DEG = 65.0;
   private static final double LEVEL_THREE_POSITION_DEG = 59.0;
   private static final double LEVEL_TWO_POSITION_DEG = -61.0;
   private static final double LEVEL_ONE_POSITION_DEG = -59.0;
