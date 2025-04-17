9ec68cb38c087dfc23166312999e0ae98ec9b775
NolanKnight <nolan.r.knight@gmail.com>
2025-04-17 13:20:55 -0500

    tuned arm level one angle

--- src/main/java/com/team973/frc2025/subsystems/Arm.java
+++ src/main/java/com/team973/frc2025/subsystems/Arm.java
@@ -32,7 +32,7 @@ public class Arm implements Subsystem {
   private static final double LEVEL_FOUR_POSITION_DEG = 65.0;
   private static final double LEVEL_THREE_POSITION_DEG = 59.0;
   private static final double LEVEL_TWO_POSITION_DEG = -61.0;
-  private static final double LEVEL_ONE_POSITION_DEG = -62.0;
+  private static final double LEVEL_ONE_POSITION_DEG = -64.0;
   public static final double CORAL_STOW_POSITION_DEG = -88.0;
 
   private static final double NET_POSITION_DEG = 74.0;
