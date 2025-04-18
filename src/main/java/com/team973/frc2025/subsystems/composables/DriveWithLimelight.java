ee9df233b975da0598e7fa686e7804560148d0fb
NolanKnight <nolan.r.knight@gmail.com>
2025-04-18 10:24:36 -0500

    slowed drive with limelight controllers back down

--- src/main/java/com/team973/frc2025/subsystems/composables/DriveWithLimelight.java
+++ src/main/java/com/team973/frc2025/subsystems/composables/DriveWithLimelight.java
@@ -282,10 +282,10 @@ public class DriveWithLimelight extends DriveComposable {
     double controlPeriodSeconds = 1.0 / RobotInfo.DriveInfo.STATUS_SIGNAL_FREQUENCY;
     m_xController =
         new ProfiledPIDController(
-            4.0, 0, 0, new TrapezoidProfile.Constraints(1.76, 0.44), controlPeriodSeconds);
+            4.0, 0, 0, new TrapezoidProfile.Constraints(1.6, 0.4), controlPeriodSeconds);
     m_yController =
         new ProfiledPIDController(
-            4.0, 0, 0, new TrapezoidProfile.Constraints(1.76, 0.44), controlPeriodSeconds);
+            4.0, 0, 0, new TrapezoidProfile.Constraints(1.6, 0.4), controlPeriodSeconds);
     m_thetaController =
         new ProfiledPIDController(
             8.0, 0, 0, new TrapezoidProfile.Constraints(1.6, 0.35), controlPeriodSeconds);
