842e6101c158f730963c5abaaa8e9fe44134e1d4
NolanKnight <nolan.r.knight@gmail.com>
2025-11-04 20:20:23 -0800

    fixed coral stow mismatch between robot info and config.yaml

--- src/main/java/com/team973/frc2025/shared/RobotInfo.java
+++ src/main/java/com/team973/frc2025/shared/RobotInfo.java
@@ -164,8 +164,8 @@ public class RobotInfo {
     public double LEVEL_ONE_POSITION_DEG = 4.0;
     public double CORAL_STOW_POSITION_DEG = -18.0;
 
-    public final double WITHOUT_CORAL_STOW_POSITION_DEG = -22.0;
-    public final double WITH_CORAL_STOW_POSTION_DEG = 0.0;
+    public double WITHOUT_CORAL_STOW_POSITION_DEG = -22.0;
+    public double WITH_CORAL_STOW_POSTION_DEG = 0.0;
 
     public double NET_POSITION_DEG = -45.0; // -20.0;
     public double ALGAE_HIGH_POSITION_DEG = -149.0;
