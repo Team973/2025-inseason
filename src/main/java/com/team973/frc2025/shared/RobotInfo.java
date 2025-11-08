ecc24542fac049b503f530d4f5a156cec0b63a70
cameron <cameron.breitung@gmail.com>
2025-11-08 13:50:46 -0800

    Update at 'Sat Nov 08 13:50:46 PST 2025'

--- src/main/java/com/team973/frc2025/shared/RobotInfo.java
+++ src/main/java/com/team973/frc2025/shared/RobotInfo.java
@@ -164,7 +164,7 @@ public class RobotInfo {
     public double LEVEL_ONE_POSITION_DEG = 4.0;
     public double CORAL_STOW_POSITION_DEG = -17.0;
 
-    public double WITHOUT_CORAL_STOW_POSITION_DEG = -20.0;
+    public double WITHOUT_CORAL_STOW_POSITION_DEG = -15.0;
     public double WITH_CORAL_STOW_POSTION_DEG = 0.0;
 
     public double NET_POSITION_DEG = -45.0; // -20.0;
