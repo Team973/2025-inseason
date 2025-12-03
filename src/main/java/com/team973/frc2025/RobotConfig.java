48872333c5a17dc4891d782955519f91bf57f389
Andrew Nelson <andrew2nelson@MacBook-Air.local>
2025-12-02 19:19:42 -0800

    RobotConfig now merges multiple config files
    This means we can keep the simulation-specific overrides in their
    own config file and we don't have to check for simulation in code
    that should really just be grabbing config.

--- src/main/java/com/team973/frc2025/RobotConfig.java
+++ src/main/java/com/team973/frc2025/RobotConfig.java
@@ -1,55 +1,80 @@
 package com.team973.frc2025;
 
+import com.fasterxml.jackson.databind.*;
 import com.team973.frc2025.shared.RobotInfo;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotBase;
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.error.MarkedYAMLException;
-import org.yaml.snakeyaml.error.YAMLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RobotConfig {
 
   private static boolean m_initialized;
   private static RobotInfo m_robotInfo;
 
+  public static enum ConfigSource {
+    Common, // default options for all robots.
+
+    // TODO: It would be nice to have overrides for red vs blue alliance
+    // but we need this data at init time and we don't get the alliance
+    // until the robot connects to the FMS.
+
+    Simulation, // overrides for when running in simulation
+    Real, // overrides for the real robot
+  }
+
   public static synchronized RobotInfo get() {
     if (!m_initialized) {
-      loadRobotInfo();
+      List<ConfigSource> configSources = getConfigSources();
+      System.err.printf("Using configSources: %s\n", configSources);
+      m_robotInfo = getMergedConfig(getConfigSources());
+      m_initialized = true;
     }
     return m_robotInfo;
   }
 
-  private static void loadRobotInfo() {
-    String filePath = Filesystem.getDeployDirectory().toPath().resolve("Config.yaml").toString();
+  /** configSuffix is something like "common", "simulation", */
+  private static String absPathForConfigSource(ConfigSource configSource) {
+    return Filesystem.getDeployDirectory()
+        .toPath()
+        .resolve("config-" + configSource.toString().toLowerCase() + ".json")
+        .toString();
+  }
 
-    try {
-      // Load YAML file into a string
-      String yamlContent = new String(Files.readAllBytes(Paths.get(filePath)), "UTF-8");
-      Yaml yaml = new Yaml();
-      m_robotInfo = yaml.loadAs(yamlContent, RobotInfo.class);
-      m_initialized = true;
+  public static List<ConfigSource> getConfigSources() {
+    List<ConfigSource> res = new ArrayList<>();
+    res.add(ConfigSource.Common);
 
-    } catch (MarkedYAMLException e) {
-      System.err.println("YAML parsing error:");
-      System.err.println("  " + e.getMessage());
-      if (e.getProblemMark() != null) {
-        System.err.println(
-            "  at line "
-                + (e.getProblemMark().getLine() + 1)
-                + ", column "
-                + (e.getProblemMark().getColumn() + 1));
-      }
-      throw e; // rethrow if you want program to stop
-
-    } catch (YAMLException e) {
-      System.err.println("General YAML parsing error: " + e.getMessage());
-      throw e;
-
-    } catch (IOException e) {
-      System.err.println("Could not read file: " + e.getMessage());
-      throw new RuntimeException(e);
+    if (RobotBase.isSimulation()) {
+      res.add(ConfigSource.Simulation);
+    } else {
+      res.add(ConfigSource.Real);
     }
+
+    return res;
+  }
+
+  public static RobotInfo getMergedConfig(List<ConfigSource> configSources) {
+    RobotInfo res = new RobotInfo();
+
+    for (ConfigSource src : configSources) {
+
+      ObjectMapper mapper = new ObjectMapper();
+      mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+      mapper.setDefaultMergeable(true);
+      try {
+        mapper.readerForUpdating(res).readValue(new File(absPathForConfigSource(src)));
+      } catch (com.fasterxml.jackson.core.JsonParseException e) {
+        System.err.printf("YAML parsing error: %s\n", e.getMessage());
+        throw new RuntimeException(e);
+      } catch (IOException e) {
+        System.err.println("Could not read file: " + e.getMessage());
+        throw new RuntimeException(e);
+      }
+    }
+
+    return res;
   }
 }
--- src/test/java/ConfigFileTest.java
+++ src/test/java/ConfigFileTest.java
@@ -2,15 +2,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.team973.frc2025.RobotConfig;
 import com.team973.frc2025.shared.RobotInfo;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.yaml.snakeyaml.Yaml;
 
 public class ConfigFileTest {
+  /**
+   * Get the config using the same codepath that the robot will use (RobotConfig.get()).
+   *
+   * We expect that tests are always running in simulation mode so we will also test that the
+   * swerve module offsets are 0 (since they should always be zero in simulation).
+   */
   @Test
   void testGetConfig() throws Exception {
     RobotInfo robotInfo = RobotConfig.get();
     System.out.printf("Config: %s\n", (new Yaml().dump(robotInfo)));
     System.out.printf("ELEVATOR_V_KP is %f\n", robotInfo.ELEVATOR_INFO.ELEVATOR_V_KP);
     assertEquals(125.0, robotInfo.ELEVATOR_INFO.ELEVATOR_V_KP, 0.01);
+    assertEquals(0.0, robotInfo.DRIVE_INFO.BACK_LEFT_MODULE_STEER_OFFSET, 0.001);
+  }
+
+  /**
+   * This test really just checks to see that all the config files can be parsed. There's no
+   * scenario IRL where we would merge _every_ config file because some of them are mutually
+   * exclusive.
+   */
+  @Test
+  void testAllConfigPermutations() throws Exception {
+    List<RobotConfig.ConfigSource> allConfigSources = List.of(RobotConfig.ConfigSource.values());
+    RobotConfig.getMergedConfig(allConfigSources);
   }
 }
